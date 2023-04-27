3a5da251fbac2c4f4417c0ff4790424607031b06
GianbaBotassi <gianba89@gmail.com>
2023-04-27 20:24:28 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -1,30 +1,27 @@
 // Declare global var
-let sumToPay;   
 const kmPrice = 0.21;   // Price/km 0,21€
-const discountYounger = 0.2;    // Discount 20% <18 years old
-const discountOlder = 0.4;  // Discount 40% >= years old
+let discount;   // discount var 
+
+// Ask user distance wanna cover
+const distance = parseInt(prompt("Inserisci la distanza che vuoi percorrere (km)."));
 
 // Ask user age
 const userAge = parseInt(prompt("Benvenuto, inserisci la tua età.")); 
 
-// Ask user distance wanna cover
-const distance = parseInt(prompt("Inserisci la distanza che vuoi percorrere (km)."))
-
-//Declare and Calculate ticket price with and without discount
-const amount = (distance * kmPrice);
-const amountYounger = amount - (amount * discountYounger);
-const amountOlder = amount - (amount * discountOlder);
-
 // Condition for discount
-if(userAge < 18){   // if under 18 years old 20%
-    sumToPay = amountYounger;
-}else if(userAge >= 65){    // else if over 65 years old 40%
-    sumToPay = amountOlder;
+if(userAge < 18){   // if under 18 years old 20% discount
+    discount = 0.2;
+}else if(userAge >= 65){    // else if over 65 years old 40% discount
+    discount = 0.4;
 }else{  // else no discount
-    sumToPay = amount;
+    discount = 0;
 }
 
-// Inner to html price to pay
+//Declare and calculate ticket price with possible discount
+const amount = (distance * kmPrice);
+const sumToPay = amount - (amount * discount);
+
+// Inner to html details and sum to pay
 document.getElementById("age").innerHTML = userAge;
 document.getElementById("distance").innerHTML = distance;
 document.getElementById("ticket_price").innerHTML = sumToPay.toFixed(2);
